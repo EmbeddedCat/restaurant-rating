7a0b5fffa0ddcb5e88b9c03be83d224f21c61f3d
Constantinos Argyriou <constarg@pm.me>
2022-07-08 01:39:39 +0300

    add admin and restaurant user

---
+++ src/main/java/uni/exercise/users/RestaurantOwner.java
@@ -0,0 +1,10 @@
+package uni.exercise.users;
+
+import uni.exercise.users.user_exceptions.UserNotFound;
+
+public class RestaurantOwner extends User {
+    @Override
+    public void login(String username, String pass) throws UserNotFound {
+
+    }
+}
